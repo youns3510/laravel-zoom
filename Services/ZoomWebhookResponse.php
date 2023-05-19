3a5adc446273d42e02c52ae71131693469c2f033
Younes Mahmoud <40076632+youns3510@users.noreply.github.com>
2023-05-19 15:37:54 +0300

    Update ZoomWebhookResponse.php
    fix errors and updated to handle any response

--- Services/ZoomWebhookResponse.php
+++ Services/ZoomWebhookResponse.php
@@ -11,10 +11,15 @@ class ZoomWebhookResponse implements RespondsToWebhook
 {
   public function respondToValidWebhook(Request $request, WebhookConfig $config): Response
   {
-    $token = json_decode($request->getContent(), true)['payload']['plainToken'];
-    return response()->json([
-      "plainToken" => $token,
-      "encryptedToken" => hash_hmac('sha256', $token, $config->signingSecret)
-    ]);
+    if (json_decode($request->getContent(), true)['event'] === 'endpoint.url_validation') {
+      $token = json_decode($request->getContent(), true)['payload']['plainToken'];
+      return response()->json([
+        "plainToken" => $token,
+        "encryptedToken" => hash_hmac('sha256', $token, $config->signingSecret)
+      ]);
+    }
+
+
+    return response()->json(['message' => 'ok']);
   }
 }
